7f8a9c908fd4a82e84a7e984eaeb86795d9e3acf
Krzysztof Bistram <bistram.k@gmail.com>
2024-10-30 13:12:33 +0100

    Created three separate angular apps

---
+++ billing/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'billing',
+      pathMatch: 'full'
+    },
+    {
+      path: 'billing',
+      loadChildren: () => import('./billing/billing.module').then(m =>
+   m.BillingModule)}]
